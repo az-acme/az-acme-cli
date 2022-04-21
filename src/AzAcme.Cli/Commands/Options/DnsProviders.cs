378bc06abb31243da860f10d4a2a0eb9a677fb51
Ross McDermott <43739066+ross-mcdermott@users.noreply.github.com>
2022-04-21 19:47:44 +1000

    Add lazy loading for DNS client, issue #4

---
+++ src/AzAcme.Cli/Commands/Options/DnsProviders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzAcme.Cli.Commands.Options
+{
+    public enum DnsProviders
+    {
+        AzureDns
+    }
+}
