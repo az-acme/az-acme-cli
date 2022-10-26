da195755ed352a4d88229e2fd3b8d23a75747172
Charrany <mortadhabi13@gmail.com>
2022-10-26 16:40:40 +0200

    feat(dns): add Azure Private DNS provider
    With this feature, azacme would be able to handle private certificate
    issuing and so could be integrated to a private PKI.
    To achieve this, the access permissions to the private zones along side
    the Key vault have to be set to azacme.
    The server option is set to the private ACME server which also have the
    permissions to access to the private zones.
    The dns provider option is set to AzurePrivate and the dns zone is
    set to a private one.
    When all these conditions are met, the user is able to order or renew
    private certifcates very easily.

---
+++ src/AzAcme.Core/Providers/AzurePrivateDns/AzurePrivateDnsZone.cs
@@ -0,0 +1,109 @@
+﻿using AzAcme.Core.Exceptions;
+using AzAcme.Core.Providers.Helpers;
+using AzAcme.Core.Providers.Models;
+using Microsoft.Azure.Management.PrivateDns;
+using Microsoft.Azure.Management.PrivateDns.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Extensions.Logging;
+
+namespace AzAcme.Core.Providers.AzurePrivateDns
+{
+    public class AzurePrivateDnsZone : IDnsZone
+    {
+        private readonly ILogger logger;
+        private readonly PrivateDnsManagementClient client;
+        private ResourceId azureDnsResource;
+        private string zoneName;
+
+        public AzurePrivateDnsZone(ILogger logger, PrivateDnsManagementClient client, string azureDnsZoneResourceId, string? zoneOverride)
+        {
+            this.azureDnsResource = ResourceId.FromString(azureDnsZoneResourceId);
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+            this.client.SubscriptionId = azureDnsResource.SubscriptionId;
+            this.zoneName = !string.IsNullOrEmpty(zoneOverride) ? zoneOverride : this.azureDnsResource.Name;
+            
+        }
+
+        public async Task<Order> SetTxtRecords(Order order)
+        {
+            // determine the TXT records needed first, so we validate all before applying.
+            foreach(var challenge in order.Challenges)
+            {
+                var record = DnsHelpers.DetermineTxtRecordName(challenge.Identitifer, this.zoneName);
+                challenge.SetRecordName(record);
+            }
+
+            // add them all
+            foreach (var challenge in order.Challenges)
+            {
+                await UpdateTxtRecord(challenge);
+            }
+
+            return order;
+        }
+
+        public async Task<Order> RemoveTxtRecords(Order order)
+        {
+            // update the record first, so we validate all.
+            foreach (var challenge in order.Challenges)
+            {
+                await RemoveTxtRecord(challenge);
+            }
+
+            return order;
+        }
+
+        private async Task RemoveTxtRecord(DnsChallenge challenge)
+        {
+            // Load all then filter
+            var recordSets = await client.RecordSets.ListByTypeAsync(azureDnsResource.ResourceGroupName, azureDnsResource.Name, Microsoft.Azure.Management.PrivateDns.Models.RecordType.TXT);
+
+            // ReSharper disable once ReplaceWithSingleCallToFirstOrDefault
+            var records = recordSets.Where(x => x.Name == challenge.TxtRecord).FirstOrDefault();
+            
+            if (records == null)
+            {
+                this.logger.LogDebug("No TXT record set for '{0}' found. Skipping delete.", challenge.TxtRecord);
+            }
+            else
+            {
+                this.logger.LogDebug("Removing TXT record set '{0}'.", challenge.TxtRecord);
+                await client.RecordSets.DeleteAsync(azureDnsResource.ResourceGroupName, azureDnsResource.Name, RecordType.TXT, challenge.TxtRecord);
+            }
+        }
+
+        private async Task UpdateTxtRecord(DnsChallenge challenge)
+        {
+            var recordSets = await client.RecordSets.ListByTypeAsync(azureDnsResource.ResourceGroupName, azureDnsResource.Name, Microsoft.Azure.Management.PrivateDns.Models.RecordType.TXT);
+
+            // ReSharper disable once ReplaceWithSingleCallToFirstOrDefault
+            var records = recordSets.Where(x => x.Name == challenge.TxtRecord).FirstOrDefault();
+
+            if (records == null)
+            {
+                this.logger.LogDebug("DNS Records do not exist for '{0}'. Creating.",challenge.TxtRecord);
+                records = new RecordSet();
+                records.Ttl = 60;
+                records.TxtRecords = new List<TxtRecord> { new TxtRecord(new List<string>() { challenge.TxtValue }) };
+
+                var result = client.RecordSets.CreateOrUpdate(azureDnsResource.ResourceGroupName, azureDnsResource.Name, RecordType.TXT, challenge.TxtRecord, records);
+            }
+            else
+            {
+                if (!(records.TxtRecords.Any(txt => txt.Value.Any(val => val == challenge.TxtValue))))
+                {
+                    this.logger.LogDebug("Updating DNS Record for '{0}'.", challenge.TxtRecord);
+                    records.TxtRecords.Add(new TxtRecord(new List<string>() { challenge.TxtValue }));
+                    var result = client.RecordSets.Update(azureDnsResource.ResourceGroupName, azureDnsResource.Name, RecordType.TXT, challenge.TxtRecord, records);
+                }
+                else
+                {
+                    this.logger.LogDebug("DNS Record with matching challenge value already exisits for '{0}'. Skipping.",challenge.TxtRecord);
+                }
+            }
+            
+        }
+
+    }
+}
